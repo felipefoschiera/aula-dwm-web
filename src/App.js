49a46dc6ab733c236a57326b858b279c2554c2df
Felipe <felipefoschiera@gmail.com>
2020-09-17 23:10:12 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import PessoasList from './PessoasList.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      lista: [
+        { nome: 'fulano', email: 'fulano@teste', idade: 30 },
+        { nome: 'beltrano', email: 'beltrano@teste', idade: 20 }
+      ]
+    }
+
+
+  }
+
+  onClickAtualizar() {
+    
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h4>Listagem de pessoas</h4>
+
+       
+        <PessoasList />
+      
+      </div>
+    )
+  }
 }
 
 export default App;
